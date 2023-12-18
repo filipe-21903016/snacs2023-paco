b587b0a1c27ed665f7c7d814266f5c3eb9521fbd
fzarco <filipezarcocoutinho@gmail.com>
2023-12-18 02:17:11 +0100

    final

---
+++ src/README.md
@@ -0,0 +1,60 @@
+# README.md
+
+## Running the Causal Paths Script and PaCo Algorithm
+
+This repository contains two scripts: `baseline.py` for calculating causal paths using pathpy and `main.py` for executing the PaCo algorithm. Follow the instructions below to run the PaCo algorithm.
+
+1. Run the PaCo algorithm with the following command:
+
+```bash
+python main.py -d <max_time_difference> [--hour/--minute] -K <max_path_length>
+```
+
+#### Parameters
+
+- `-d` or `--delta`: Maximum time difference between consecutive interactions. Must be specified.
+- `--hour`: If present, the time difference will be interpreted in hours.
+- `--minute`: If present, the time difference will be interpreted in minutes.
+  - If neither `--hour` nor `--minute` is specified, `delta` is interpreted in seconds.
+- `-K`: Maximum path length. Must be specified.
+
+### Example
+
+```bash
+python main.py -d 10 --minute -K 3
+```
+
+This example sets the maximum time difference to 10 minutes and the maximum path length to 3.
+
+### Output
+
+The script will output the total number of causal paths found by the PaCo algorithm and the runtime.
+
+For further assistance, please contact [your-email@example.com].
+
+**Note:** This README assumes that you have Python installed on your system. README.md
+
+## Running the Baseline Script
+
+
+1. Run the script with the following command:
+
+```bash
+python baseline.py -d <max_time_difference> [--hour/--minute] -K <max_path_length>
+```
+
+#### Parameters
+
+- `-d` or `--delta`: Maximum time difference between consecutive interactions. Must be specified.
+- `--hour`: If present, the time difference will be interpreted in hours.
+- `--minute`: If present, the time difference will be interpreted in minutes.
+  - If neither `--hour` nor `--minute` is specified, `delta` is interpreted in seconds.
+- `-K`: Maximum path length. Must be specified.
+
+### Example
+
+```bash
+python baseline.py -d 10 --minute -K 3
+```
+
+This example sets the maximum time difference to 10 minutes and the maximum path length to 3.
---
+++ src/baseline.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+import pandas as pd
+import pathpy as pp
+from tqdm import tqdm
+from time import time
+import argparse
+import os
+
+LOGS_DIR = 'logs'
+DATASET_PATH = "./reddit_hyperlink_network.tsv"
+
+# Generate a timestamp for the log file
+timestamp = datetime.now().strftime("%Y%m%d%H%M")
+
+def load_edgelist():
+    def date_to_timestamp(date_string: str):
+        return int(datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S").timestamp())
+
+    reddit = pd.read_csv(DATASET_PATH, delimiter='\t')
+
+    data = data = reddit[['SOURCE_SUBREDDIT', 'TARGET_SUBREDDIT', 'TIMESTAMP']]
+    data.columns = ['source', 'target', 'timestamp']
+
+    return [(row['source'], row['target'], date_to_timestamp(row['timestamp'])) for _, row in data.iterrows()]
+
+
+def path_count(paths, k):
+  count = 0
+  for k in range(1, k + 1):
+      count += sum([v.sum() for v in paths.paths[k].values()])
+  return count
+
+def get_total_causal_paths(temporal_network, delta, K):
+    start = time()
+    # Calculate the number of causal paths
+    paths = pp.path_extraction.temporal_paths.paths_from_temporal_network_dag(temporal_network, delta=delta)
+
+    output = f"Number of causal paths: {paths}\n"
+    print(output)
+    
+    count = path_count(paths, K)
+    end = time()
+    runtime = end - start
+    
+    output += f"Total causal paths count: {count}\n"
+    output += f"Runtime: {runtime} seconds\n"
+    
+    # Log the program output to a file with a unique name
+    log_file_name = f"{LOGS_DIR}/baseline_{timestamp}_delta{delta}_K{K}.log"
+    with open(log_file_name, 'w') as log_file:
+        log_file.write(output)
+    
+    return count, runtime
+
+def create_temporal_network(edge_list):
+    # Create a temporal network
+    t = pp.TemporalNetwork()
+    for edge in tqdm(edge_list):
+        s, d, ts = edge
+        t.add_edge(s, d, ts)
+    return t
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Process command line arguments.")
+    parser.add_argument(
+        "-d", type=int, help="Maximum time difference", required=True)
+    parser.add_argument(
+        "--hour",dest='h' , help="Set time difference to hours", action='store_true')
+    parser.add_argument(
+        "--minute",dest='m',  help="Set time difference to minutes", action='store_true')
+    parser.add_argument(
+        "-K", type=int, help="Maximum path length", required=True)
+    args = parser.parse_args()
+
+    delta = args.d * (60 if args.m is True or args.h is True else 1) * (60 if args.h is True else 1) 
+
+    edge_list = load_edgelist()
+    t = create_temporal_network(edge_list)
+
+    # Create the 'logs' directory if it doesn't exist
+    if not os.path.exists(LOGS_DIR):
+        os.makedirs(LOGS_DIR)
+
+    count, runtime = get_total_causal_paths(t, delta, args.K)
+    print(f'Pathpy found {count} causal paths. ({round(runtime, 2)}s)')
